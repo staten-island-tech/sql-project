81ff1d11c4a2a50e622eca2985855917186c6775
JunxiC <chenjon09@gmail.com>
2023-05-10 10:13:20 -0400

    kimchi muncher

--- sql-project/src/App.vue
+++ sql-project/src/App.vue
@@ -1,3 +1,13 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 </script>
+
+<template>
+  <h1 class="main">Hi</h1>
+</template>
+
+<style>
+.main {
+  background-color: purple;
+}
+</style>
