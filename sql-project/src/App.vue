be6e0ba1f6369c82e71e688af7099b879d85b83c
MinulGamage <113190129+MinulGamage@users.noreply.github.com>
2023-05-12 09:42:54 -0400

    cummit 4

--- sql-project/src/App.vue
+++ sql-project/src/App.vue
@@ -6,6 +6,10 @@ import { RouterLink, RouterView } from 'vue-router'
   <h1 class="header"></h1>
   <nav>
     <RouterLink class="link-home" to="/">Home</RouterLink>
+    <RouterLink class="link-home" to="/about">About Us</RouterLink>
+    <RouterLink class="link-home" to="/HousekeepersView">Housekeepers</RouterLink>
+    <RouterLink class="link-home" to="/GardernersView">Gardeners</RouterLink>
+    <RouterLink class="link-home" to="/CooksView">Cooks</RouterLink>
   </nav>
   <nav>
     <RouterLink class="link-Login" to="/loginView">Log In</RouterLink>
