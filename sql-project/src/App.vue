d7f97f2a220dd77f9807b0849de06bdeb2ee6b9f
JunxiC <chenjon09@gmail.com>
2023-05-12 09:47:42 -0400

    cumminit 5

--- sql-project/src/App.vue
+++ sql-project/src/App.vue
@@ -3,18 +3,30 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <h1 class="header"></h1>
-  <nav>
-    <RouterLink class="link-home" to="/">Home</RouterLink>
-    <RouterLink class="link-home" to="/about">About Us</RouterLink>
-    <RouterLink class="link-home" to="/HousekeepersView">Housekeepers</RouterLink>
-    <RouterLink class="link-home" to="/GardernersView">Gardeners</RouterLink>
-    <RouterLink class="link-home" to="/CooksView">Cooks</RouterLink>
-  </nav>
-  <nav>
-    <RouterLink class="link-Login" to="/loginView">Log In</RouterLink>
-    <RouterLink class="link-SignUp" to="/SignUpView">Sign Up</RouterLink>
-  </nav>
+  <div>
+    <h1 class="header"></h1>
+    <nav>
+      <RouterLink class="link-home" to="/">Home</RouterLink>
+    </nav>
+    <nav>
+      <RouterLink class="link-home" to="/about">About Us</RouterLink>
+    </nav>
+    <nav>
+      <RouterLink class="link-home" to="/HousekeepersView">Housekeepers</RouterLink>
+    </nav>
+    <nav>
+      <RouterLink class="link-home" to="/GardernersView">Gardeners</RouterLink>
+    </nav>
+    <nav>
+      <RouterLink class="link-home" to="/CooksView">Cooks</RouterLink>
+    </nav>
+    <nav>
+      <RouterLink class="link-Login" to="/loginView">Log In</RouterLink>
+    </nav>
+    <nav>
+      <RouterLink class="link-SignUp" to="/SignUpView">Sign Up</RouterLink>
+    </nav>
+  </div>
 </template>
 
 <style>
