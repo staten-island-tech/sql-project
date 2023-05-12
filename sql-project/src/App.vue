ecc282b65d9ebf6ec4187479781fd0fbd463c295
JunxiC <chenjon09@gmail.com>
2023-05-12 10:13:30 -0400

    commmmmmm

--- sql-project/src/App.vue
+++ sql-project/src/App.vue
@@ -3,34 +3,36 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div>
+  <div class="head">
     <h1 class="header"></h1>
     <nav>
-      <RouterLink class="link-home" to="/">Home</RouterLink>
+      <RouterLink class="link head" to="/">Home</RouterLink>
     </nav>
     <nav>
-      <RouterLink class="link-home" to="/about">About Us</RouterLink>
+      <RouterLink class="link head" to="/about">About Us</RouterLink>
     </nav>
     <nav>
-      <RouterLink class="link-home" to="/HousekeepersView">Housekeepers</RouterLink>
+      <RouterLink class="link head" to="/HousekeepersView">Housekeepers</RouterLink>
     </nav>
     <nav>
-      <RouterLink class="link-home" to="/GardernersView">Gardeners</RouterLink>
+      <RouterLink class="link head" to="/GardernersView">Gardeners</RouterLink>
     </nav>
     <nav>
-      <RouterLink class="link-home" to="/CooksView">Cooks</RouterLink>
+      <RouterLink class="link head" to="/CooksView">Cooks</RouterLink>
     </nav>
     <nav>
-      <RouterLink class="link-Login" to="/loginView">Log In</RouterLink>
+      <RouterLink class="link-Login head" to="/loginView">Log In</RouterLink>
     </nav>
     <nav>
-      <RouterLink class="link-SignUp" to="/SignUpView">Sign Up</RouterLink>
+      <RouterLink class="link-SignUp head" to="/SignUpView">Sign Up</RouterLink>
     </nav>
   </div>
 </template>
 
 <style>
-.header {
+.head {
   color: #f4f1d6;
+  display: inline-block;
+  flex-direction: row;
 }
 </style>
