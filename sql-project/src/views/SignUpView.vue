4e6d552d6c8f810b85983fda8fd4e237f10becfd
JunxiC <chenjon09@gmail.com>
2023-05-17 10:10:19 -0400

    cummit kms 17

--- sql-project/src/views/SignUpView.vue
+++ sql-project/src/views/SignUpView.vue
@@ -1,3 +1,21 @@
 <script></script>
-<template></template>
-<script></script>
+<template>
+  <form class="loginForm">
+    <div class="email">
+      <input type="text" id="emailID" placeholder="Enter Email" />
+    </div>
+    <div>
+      <input type="text" id="passwordID" placeholder="Enter Password" />
+    </div>
+    <div>
+      <input type="text" id="confirmpasswordID" placeholder="Confirm Password" />
+    </div>
+    <button id="loginbtn">Login</button>
+  </form>
+</template>
+
+<style>
+.loginForm {
+  border: black 3px solid;
+}
+</style>
