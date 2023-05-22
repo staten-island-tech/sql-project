a48b23f268735361175a015a3c0780041c4b9820
JunxiC <chenjon09@gmail.com>
2023-05-22 10:16:10 -0400

    cummit 18

--- sql-project/src/views/AboutView.vue
+++ sql-project/src/views/AboutView.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Hey Sisters</h1>
+    <h4>
+      This is E-House, a platform where you can interact and buy services from certified
+      professionals in your community.
+    </h4>
+    <h2>Our Services</h2>
+    <h4>1. Become an E-Houser</h4>
+    <h5>You can rent out your services to those around you in a safe and kind community</h5>
+    <h4>2. Become an E-Renter</h4>
+    <h5>You can rent one of our million E-Housers</h5>
   </div>
 </template>
 
