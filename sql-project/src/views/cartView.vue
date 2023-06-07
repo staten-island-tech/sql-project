413aa6e225c1dc2a52026977bcba713e036a8ca3
MinulGamage <113190129+MinulGamage@users.noreply.github.com>
2023-06-07 10:16:04 -0400

    cummit 4003q0441

--- sql-project/src/views/cartView.vue
+++ sql-project/src/views/cartView.vue
@@ -1,4 +1,15 @@
-<script></script>
+<script>
+import { service } from './boxTemplate.vue'
+</script>
 <template>
   <h3 class="cart-header">Your Cart</h3>
+
+  <div class="CartBox">
+    <h2 class="name">{{ name }}</h2>
+    <img class="image" v-bind:src="img" v-bind:alt="img" />
+    <h3 class="price">Price: ${{ price }}</h3>
+    <button @click="removefromShoppingCart({ species: species })" class="Remove">
+      Remove from Cart
+    </button>
+  </div>
 </template>
